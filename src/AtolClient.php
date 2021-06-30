1176487e1b7a45e1efae64f7e9a4a9227c9d86e7
Vladimir <vhar69@gmail.com>
2021-06-30 11:56:23 +0300

    Замена дефиса на нижнее подчеркивание в логине при формировании токена

--- src/AtolClient.php
+++ src/AtolClient.php
@@ -121,7 +121,7 @@ class AtolClient
 
   private function getToken(string $login, string $password): string
   {
-    $CACHE_KEY = 'atol.token' . $login;
+    $CACHE_KEY = 'atol.token' . str_replace('-', '_' ,$login);
     if($this->cache->has($CACHE_KEY)) {
       return (string)$this->cache->get($CACHE_KEY);
     }
